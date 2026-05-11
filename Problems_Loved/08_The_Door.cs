41a5d9ff08a92aa42fe9f83b36143cb8ba217984
Aashish Raj <iamtheaashish@gmail.com>
2026-05-11 10:15:53 +0530

    Refactor door state management and simplify ChangeDoorState method

--- Problems_Loved/08_The_Door.cs
+++ Problems_Loved/08_The_Door.cs
@@ -1,5 +1,7 @@
 Door Steel = new Door("sexy69");
 
+Console.Clear();
+
 while (true)
 {
     // Console.Clear();
@@ -7,14 +9,14 @@ while (true)
     Console.WriteLine("Press: 1. Open, 2. Close, 3. Lock, 4. Change Passcode");
     int input = userInput();
 
-    if (input == 1 && Steel.CurrentState == DoorState.Closed) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Open); // Open if the door is closed.
-    if (input == 1 && Steel.CurrentState == DoorState.Locked) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Open); // Open if the door is closed and locked.
+    if (input == 1 && Steel.CurrentState == DoorState.Closed) Steel.ChangeDoorState(DoorState.Open); // Open if the door is closed.
+    if (input == 1 && Steel.CurrentState == DoorState.Locked) Steel.ChangeDoorState(DoorState.Open); // Open if the door is closed and locked.
 
-    if (input == 2 && Steel.CurrentState == DoorState.Open) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Closed); // Close if the door is open.
-    if (input == 2 && Steel.CurrentState == DoorState.Locked) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Closed); // Unlock the door, but keep it closed.
+    if (input == 2 && Steel.CurrentState == DoorState.Open) Steel.ChangeDoorState(DoorState.Closed); // Close if the door is open.
+    if (input == 2 && Steel.CurrentState == DoorState.Locked) Steel.ChangeDoorState(DoorState.Closed); // Unlock the door, but keep it closed.
 
-    if (input == 3 && Steel.CurrentState == DoorState.Open) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Locked); // Lock if the door is open.
-    if (input == 3 && Steel.CurrentState == DoorState.Closed) Steel.ChangeDoorState(Steel.CurrentState, DoorState.Locked); // Lock if the door is closed.
+    if (input == 3 && Steel.CurrentState == DoorState.Open) Steel.ChangeDoorState(DoorState.Locked); // Lock if the door is open.
+    if (input == 3 && Steel.CurrentState == DoorState.Closed) Steel.ChangeDoorState(DoorState.Locked); // Lock if the door is closed.
 
     if (input == 4)
     {
@@ -81,22 +83,22 @@ public class Door
         }
 
     }
-    public void ChangeDoorState(DoorState currentState, DoorState desiredState)
+    public void ChangeDoorState(DoorState desiredState)
     {
-        if (currentState == DoorState.Open && desiredState == DoorState.Closed) CurrentState = DoorState.Closed;
-        if (currentState == DoorState.Open && desiredState == DoorState.Locked) CurrentState = DoorState.Locked;
+        if (CurrentState == DoorState.Open && desiredState == DoorState.Closed) CurrentState = DoorState.Closed;
+        if (CurrentState == DoorState.Open && desiredState == DoorState.Locked) CurrentState = DoorState.Locked;
 
-        if (currentState == DoorState.Closed && desiredState == DoorState.Open) CurrentState = DoorState.Open;
-        if (currentState == DoorState.Closed && desiredState == DoorState.Locked) CurrentState = DoorState.Locked;
+        if (CurrentState == DoorState.Closed && desiredState == DoorState.Open) CurrentState = DoorState.Open;
+        if (CurrentState == DoorState.Closed && desiredState == DoorState.Locked) CurrentState = DoorState.Locked;
 
-        if (currentState == DoorState.Locked && desiredState == DoorState.Open) // Unlock and open
+        if (CurrentState == DoorState.Locked && desiredState == DoorState.Open) // Unlock and open
         {
             Console.Write("The door is currently locked. Enter the passcode: ");
             string? passcodeStr = Console.ReadLine();
             if (passcodeStr != _passCode) Console.WriteLine("Sorry, the passcode was wrong. You can't unlock and open it.");
             else CurrentState = DoorState.Open;
         }
-        if (currentState == DoorState.Locked && desiredState == DoorState.Closed) // Unlock but not open
+        if (CurrentState == DoorState.Locked && desiredState == DoorState.Closed) // Unlock but not open
         {
             Console.Write("The door is currently locked. Enter the passcode: ");
             string? passcodeStr = Console.ReadLine();
