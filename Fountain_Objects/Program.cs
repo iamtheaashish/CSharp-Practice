9d2f53cae930bac8e79067157a45082a97d002b1
Aashish Raj <iamtheaashish@gmail.com>
2026-05-21 09:04:34 +0530

    Remove GameLoop class and refactor main method to use Game class; update player movement logic and map initialization.

--- Fountain_Objects/Program.cs
+++ Fountain_Objects/Program.cs
@@ -1,57 +1,85 @@
-new GameLoop().Run();
+Game game = new();
+game.Run();
 
-// ---> end of main method
-public class GameLoop
+// end of main method.
+public class Game
 {
-    public Grid Grid {get;} = new Grid();
-    public Player Player {get;} = new Player();
+    public Map Map { get; } = new();
+    public Player Player { get; } = new();
 
     public void Run()
     {
-        while(true)
+        while (true)
         {
-        Console.WriteLine("----------------------------------------------------------------------------------");
-        Console.WriteLine($"You are in the room at (Row={Player.CurrentLocation.row}, Column={Player.CurrentLocation.column}).");
-        break;
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine($"You are in the room at (Row={Player.Location.Row}, Column={Player.Location.Column}).");
+
+            MoveAround(Player);
         }
+    }
+
+    public void MoveAround(Player player)
+    {
+        Console.Write("What do you want to do? ");
+        
+        string? input = Console.ReadLine()?.ToLower();
+        
+        switch (input)
+        {
+            case "move north":
+                if (player.Location.Row > 0) player.Location.Row--;
+                break;
+            case "move south":
+                if (player.Location.Row < 3) player.Location.Row++;
+                break;
+            case "move west":
+                if (player.Location.Column > 0) player.Location.Column--;
+                break;
+            case "move east":
+                if (player.Location.Column < 3) player.Location.Column++;
+                break;
+            default: 
+                Console.WriteLine("I don't understand, try again.");
+                break;
+        }
+        
+    }
+
+}
+
+public class Map
+{
+    private Room[,] _rooms;
+
+
+    public Map()
+    {
+        _rooms = new Room[4,4];
+
+        for(int row = 0; row < 4; row++)
+            for(int column = 0; column < 4; column++)
+                _rooms[row,column] = Room.Empty;
+
+        _rooms[0,0] = Room.Enterance;
+        _rooms[0,2] = Room.Fountain;
     }
 }
 
-public class PlayerInput
-{
-    
-}
+public enum Room { Empty, Enterance, Fountain }
 
 public class Player
 {
-    public Location CurrentLocation { get; set; } = new Location(0, 0);
-    
+    public Location Location { get; set; } = new(0,0);
 }
-public record Location (int row, int column);
 
-public class Grid
+public class Location
 {
-   public Room[,] _rooms;
+    public int Row { get; set; }
+    public int Column { get; set; }
 
-    public Grid()
+    public Location(int row, int column)
     {
-        _rooms = new Room[4,4];
-        for(int row = 0; row < 4; row++)
-            for(int column = 0; column < 4; column++)
-            {
-                _rooms[row, column] = new EmptyRoom();
-            }
-
-        
-        _rooms[0, 0] = new EntranceRoom();
-        _rooms[0, 2] = new FountainRoom();
+        Row = row;
+        Column = column;
     }
-}
-
-public abstract class Room { }
-public class EmptyRoom : Room { }
-public class EntranceRoom : Room { }
-public class FountainRoom : Room
-{
-    public bool IsFlowing { get; set; }
 }
--- Program.cs
+++
@@ -1,5 +0,0 @@
-Random random = new Random();
-
-Console.WriteLine(random.NextDouble() * 10);
-
-// ---> the end of main method.
