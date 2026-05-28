9b86b17ffd4318295d21f051ea54b038984cecbe
Aashish Raj <iamtheaashish@gmail.com>
2026-05-28 09:19:47 +0530

    Add Sieve implementation: create number filtering logic using delegates and user input

---
+++ Problems_Loved/18_Delegates.cs
@@ -0,0 +1,40 @@
+Console.WriteLine("1 - Even");
+Console.WriteLine("2 - Positive");
+Console.WriteLine("3 - Multiple of 10");
+
+int input = int.Parse(Console.ReadLine()!);
+
+Sieve sieve = input switch
+{
+    1 => new Sieve(IsEven),
+    2 => new Sieve(IsPositive),
+    3 => new Sieve(IsMultiple10),
+    _ => throw new Exception("Invalid choice!"),
+};
+
+while (true)
+{
+    Console.Write("Enter a number to sieve: ");
+    int inpNum = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine(sieve.IsGood(inpNum) ? "Good" : "Bad");
+}
+
+
+bool IsEven(int x) => n % 2 == 0;
+bool IsPositive(int x) => n > 0;
+bool IsMultiple10(int x) => x % 10 == 0;
+
+public class Sieve
+{
+    private readonly Func<int, bool> _refine;
+
+    public Sieve(Func<int, bool> refine)
+    {
+        _refine = refine;
+    }
+
+    public bool IsGood(int number)
+    {
+    }
+}
